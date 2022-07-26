b8a02343a74971ea6ca2bdd56967f3af1f94ccf6
Espen Klem <espen.klem@gmail.com>
2022-07-26 15:41:53 +0200

    Removing images from content file and also updating nrk-sapmi-crawler to not crawl images anymore.

---
+++ scripts/remove_img.mjs
@@ -0,0 +1,15 @@
+// ## Just to remove img-key from arrays of objects since the selector for photos is not precise enough
+
+// import { readIfExists, writeJson } from 'nrk-sapmi-crawler'
+
+// const southSami = { file: './datasets/content.southSami.json' }
+// const luleSami = { file: './datasets/content.luleSami.json' }
+// const northSami = { file: './datasets/content.northSami.json' }
+
+
+// const readFile = await readIfExists(northSami.file)
+
+// const fileWithoutImg = readFile.map(({img, ...rest}) => rest)
+// console.log(fileWithoutImg)
+
+// writeJson(northSami.file, fileWithoutImg)
